a70118682463ed5dc135b9e882e921047249e9aa
miyad <1505074.msr@ugrad.cse.buet.ac.bd>
2021-05-17 01:56:18 +0600

    project3 done

--- control.js
+++ control.js
@@ -21,3 +21,69 @@ function do_task(name) {
     });
 }
 console.log("hello world");
+
+/*! function(d, s, id) {
+    var forcast = document.getElementById('forcast');
+    forcast.addEventListener('click', () => {
+        console.log("i am pressed");
+        var js, fjs = d.getElementsByTagName(s)[0];
+        setTimeout('', 4000);
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://weatherwidget.io/js/widget.min.js';
+            var child = fjs.parentNode.insertBefore(js, fjs);
+
+            // setTimeout(fjs.parentNode.insertBefore(js, fjs), 50);
+        } else {
+            let child = fjs.parentNode.insertBefore(js, fjs);
+            fjs.parentNode.removeChild(child);
+        }
+    });
+
+}(document, 'script', 'weatherwidget-io-js');*/
+fetch('http://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=Jhenaidah&appid=c1eae7b765f5d42bb32f09ec0ab4f232')
+    .then((response) => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        var one, two = data.weather[0];
+
+        console.log(two.main);
+        console.log(data.main.temp - 273);
+        console.log(data.main.feels_like - 273);
+        var w = document.getElementById('w');
+        w.textContent = two.main;
+        var tmp = document.getElementById('tmp');
+        tmp.textContent = Math.round(data.main.temp - 273);
+        var fl = document.getElementById('fl');
+        fl.textContent = Math.round(data.main.feels_like - 273);
+    })
+var loc = document.getElementById('loc');
+var locIcon = document.getElementById('locIcon');
+var fcast = document.getElementById('forecast');
+loc.addEventListener('click', () => {
+    if (fcast.classList.contains("hidden")) {
+        fcast.classList.remove("hidden");
+        setTimeout(fade_out, 5000);
+        //fcast.classList.add("hidden");
+
+        function fade_out() {
+            fcast.classList.add("hidden");
+        }
+
+    }
+});
+locIcon.addEventListener('click', () => {
+    if (fcast.classList.contains("hidden")) {
+        fcast.classList.remove("hidden");
+        setTimeout(fade_out, 5000);
+        //fcast.classList.add("hidden");
+
+        function fade_out() {
+            fcast.classList.add("hidden");
+        }
+
+    }
+});
